1aee86d7fee111892addb2a4ea4022c69e8c9df4
Solmarnov <solmarnov@gmail.com>
2022-01-06 10:00:30 +1100

    adds backend models

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB Connected: ${connect.connection.host}`)
+    console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.bold)
   } catch (error) {
-    console.error(`Error: ${error}`)
+    console.error(`Error: ${error}`.red.bold)
     process.exit(1)
   }
 }
