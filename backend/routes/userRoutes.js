15b2b2ba6b910adbf1e672683436e887e315a232
Solmarnov <solmarnov@gmail.com>
2022-02-05 14:10:42 +1100

    adds user authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
